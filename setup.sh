68aa160218b072bef7e2fc5b76399e61e58e8689
irizzant <i.rizzante@gmail.com>
2024-10-17 10:41:37 +0200

    feat!: add reproducer

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+kind create cluster
+
+helm upgrade --install crossplane crossplane --repo https://charts.crossplane.io/stable --namespace crossplane-system --create-namespace --wait
+
+kubectl apply -f provider-kubernetes-incluster.yaml
+
+kubectl wait --for=condition=healthy provider.pkg.crossplane.io --all --timeout=5m
+
+kubectl apply -f providerconfig.yaml
+
+kubectl apply -f functions.yaml
+
+kubectl wait --for=condition=healthy function.pkg.crossplane.io --all --timeout={{timeout}}
+
+kubectl apply -f extra-resources.yaml
+
+kubectl apply -f compositeresourcedefinition.yaml
+
+kubectl apply -f composition.yaml
+
+kubectl apply -f xr.yaml
